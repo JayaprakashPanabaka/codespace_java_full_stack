2b6e2c1f2841223408f128bdc9b84748b760c7d5
Jayaprakash Panabaka <123289779+JayaprakashPanabaka@users.noreply.github.com>
2025-04-14 10:18:56 +0000

    welcome

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Welcome from './Module 5/React Basics/Ques_1'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -6,6 +7,7 @@ function App() {
   return (
    <>
     <h1>App Component</h1>
+    <Welcome />
    </>
   )
 }
--- src/Module 5/React Basics/Ques_1.jsx
+++ src/Module 5/React Basics/Ques_1.jsx
@@ -1,2 +1,12 @@
 // Create a Simple Component
 // Description: Create a React component named Welcome that displays a message "Welcome to React!".
+
+const Welcome = () => {
+    return (
+        <div>
+            <h1>Welcome to React!</h1>
+        </div>
+    )
+}
+
+export default Welcome;
